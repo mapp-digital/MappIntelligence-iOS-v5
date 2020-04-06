a55fe68a163fcb0328b71dca2a22bcceea2e4700
Stefan Stevanovic <stefan.stevanovic@mapp.com>
2020-04-06 15:37:58 +0200

    removed unused unit tests and change fns interval to 30 mins

---
+++ MappIntelligenceTests/Trackers/UIFlowObserverTests.m
@@ -0,0 +1,9 @@
+//
+//  UIFlowObserverTests.m
+//  MappIntelligenceTests
+//
+//  Created by Stefan Stevanovic on 4/6/20.
+//  Copyright © 2020 Stefan Stevanovic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
