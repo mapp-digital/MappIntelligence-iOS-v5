e587109ca3a6d2c51c02c3eba984a15cda25d732
Stefan Stevanovic <stefan.stevanovic@mapp.com>
2020-04-07 14:01:10 +0200

    test for uiflowobserver

--- MappIntelligenceTests/Trackers/UIFlowObserverTests.m
+++ MappIntelligenceTests/Trackers/UIFlowObserverTests.m
@@ -7,3 +7,36 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <XCTest/XCTest.h>
+#import "DefaultTracker.h"
+#import "UIFlowObserver.h"
+
+
+@interface UIFlowObserverTests : XCTestCase
+
+@property DefaultTracker *tracker;
+@property UIFlowObserver *observer;
+
+@end
+
+@implementation UIFlowObserverTests
+
+- (void)setUp {
+    [super setUp];
+    _tracker = [DefaultTracker sharedInstance];
+    _observer = [[UIFlowObserver alloc] initWith:_tracker];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testInitWithTracker {
+    XCTAssertNotNil(_tracker);
+    XCTAssertNotNil(_observer);
+}
+
+- (void)testSetup {
+    XCTAssertTrue([_observer setup]);
+}
+@end
