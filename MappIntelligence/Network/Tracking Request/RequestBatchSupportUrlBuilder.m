4aee716fcbdd8a2c9696719df6e107f5daa1e35e
Stefan Stevanovic <stefan.stevanovic@miticon.com>
2020-07-08 13:09:37 +0200

    device batch on 5000 requests if there is more than 10000 requests saved into database

--- MappIntelligence/Network/Tracking Request/RequestBatchSupportUrlBuilder.m
+++ MappIntelligence/Network/Tracking Request/RequestBatchSupportUrlBuilder.m
@@ -40,25 +40,54 @@
 -(void)sendBatchForRequests {
     [_dbManager fetchAllRequestsWithCompletionHandler:^(NSError * _Nonnull error, id  _Nullable data) {
         RequestData* dt = (RequestData*)data;
-        NSString* body = [self createBatchWith:dt];
+        NSArray<NSString *>* bodies = [self createBatchWith:dt];
         TrackerRequest *request = [[TrackerRequest alloc] init];
-        [request sendRequestWith:[[NSURL alloc] initWithString:self->_baseUrl] andBody:body andCompletition:^(NSError * _Nonnull error) {
-            if (!error) {;
-                [self->_loger logObj: [[NSString alloc] initWithFormat:@"Batch request sent successfuly!"] forDescription: kMappIntelligenceLogLevelDescriptionDebug];
-                [self->_dbManager removeRequestsDB:[self getRequestIDs:dt]];
-            }
-        }];
+        for (NSString *body in bodies) {
+          [request
+              sendRequestWith:[[NSURL alloc] initWithString:self->_baseUrl]
+                      andBody:body
+              andCompletition:^(NSError *_Nonnull error) {
+                if (!error) {
+                  ;
+                  [self->_loger
+                              logObj:[[NSString alloc]
+                                         initWithFormat:
+                                             @"Batch request sent successfuly!"]
+                      forDescription:kMappIntelligenceLogLevelDescriptionDebug];
+                  [self->_dbManager removeRequestsDB:[self getRequestIDs:dt]];
+                }
+              }];
+        }
     }];
 }
 
--(NSString*)createBatchWith:(RequestData*) data {
-    NSMutableString* body = [[NSMutableString alloc] init];
-    for (Request* req in data.requests) {
-        [body appendString:@"wt?"];
-        [body appendString: [[req url] query]];
-        [body appendString:@"\n"];
+- (NSArray<NSString *> *)createBatchWith:(RequestData *)data {
+  NSMutableString *body = [[NSMutableString alloc] init];
+  int i = 0;
+  NSMutableArray *array = [[NSMutableArray alloc] init];
+  long requestsCount = data.requests.count;
+  if (requestsCount > 10000) {
+    [body setString:@""];
+    long length =
+        (requestsCount - i * 5000) > 5000 ? 5000 : (requestsCount - i * 5000);
+    NSArray *subArray =
+        [data.requests subarrayWithRange:NSMakeRange(i * 5000, length)];
+    for (Request *req in subArray) {
+      [body appendString:@"wt?"];
+      [body appendString:[[req url] query]];
+      [body appendString:@"\n"];
     }
-    return body;
+    [array addObject:body];
+    i++;
+  } else {
+    for (Request *req in data.requests) {
+      [body appendString:@"wt?"];
+      [body appendString:[[req url] query]];
+      [body appendString:@"\n"];
+    }
+    [array addObject:body];
+  }
+  return array;
 }
 
 -(NSArray* )getRequestIDs:(RequestData*) data {
