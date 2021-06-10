f556ae0a394710b00cedbe418c20cb9f69dfa0f3
Stefan <stefan.stevanovic@mapp.com>
2021-06-10 11:37:39 +0200

    update version to beta9

--- MappIntelligenceTests/Network/Tracking Request/TrackerRequestTests.m
+++ MappIntelligenceTests/Network/Tracking Request/TrackerRequestTests.m
@@ -42,14 +42,14 @@
 }
 
 - (void)testSendRequestWith {
-    NSURL *url = [[NSURL alloc] initWithString:@"https://q3.webtrekk.net/385255285199574/wt?p=500,MappIntelligenceDemoApp.ViewController,0,1125x2436,32,0,1586272855006,0,0,0&eid=6158627047292181549&fns=1&one=0&X-WT-UA=Tracking%20Library%205.0.0%20(iOS%20Version%2013.3%20(Build%2017C45);%20iPhone;%20en_US))&X-WT-IP=192.168.1.3&la=en&eor=1"];
+    NSURL *url = [[NSURL alloc] initWithString:@"https://q3.webtrekk.net/385255285199574/wt?p=500,MappIntelligenceDemoApp.ViewController,0,1125x2436,32,0,1586272855006,0,0,0&eid=6158627047292181549&fns=1&one=0&X-WT-UA=Tracking%20Library%205.0.0-beta9%20(iOS%20Version%2013.3%20(Build%2017C45);%20iPhone;%20en_US))&X-WT-IP=192.168.1.3&la=en&eor=1"];
     [_request sendRequestWith:url andCompletition:^(NSError * _Nonnull error) {
         XCTAssertNotNil(error);
     }];
 }
 
 - (void)testSendRequestWithURLAndBody {
-    NSURL *url = [[NSURL alloc] initWithString:@"http://tracker-int-01.webtrekk.net/794940687426749/batch?eid=6159533923421872647&X-WT-UA=Tracking%20Library%205.0.0%20(iOS%20Version%2013.6%20(Build%2017G64);%20iPhone;%20en_US"];
+    NSURL *url = [[NSURL alloc] initWithString:@"http://tracker-int-01.webtrekk.net/794940687426749/batch?eid=6159533923421872647&X-WT-UA=Tracking%20Library%205.0.0-beta9%20(iOS%20Version%2013.6%20(Build%2017G64);%20iPhone;%20en_US"];
     NSString* body = @"wt?p=500,testCustomStringNametestCustomStringNametestCustomStringNametestCustomStringNametestCustomStringNametestCustomStringNametestCustomStringNametestCustomStringNametestCustomStringNametestCustomStringNametestCustomStrin,0,750x1334,32,0,1596547757909,0,0,0&fns=0&one=0&X-WT-IP=192.168.1.6&la=en&eor=1\nwt?p=500,defaultName,0,750x1334,32,0,1596547763110,0,0,0&fns=0&one=0&X-WT-IP=192.168.1.6&la=en&eor=1&cp20=cp20Override&is=testSearchTerm\nwt?p=500,MappIntelligenceDemoApp.ViewController,0,750x1334,32,0,1596548558601,0,0,0&fns=0&one=0&X-WT-IP=192.168.1.6&la=en&eor=1";
     [_request sendRequestWith:url andBody: body andCompletition:^(NSError * _Nonnull error) {
         XCTAssertNotNil(error);
