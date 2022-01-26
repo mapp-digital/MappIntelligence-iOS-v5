3b7683a3d9ff17ebad2ca2ec2fac15f27a18434e
Stefan Stevanovic <stefan.stevanovic@gerniks.com>
2022-01-26 12:43:40 +0100

    added controller methods for form tracking

---
+++ MappIntelligenceDemoApp/MappIntelligenceDemoApp/FormViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FormViewController.swift
+//  MappIntelligenceDemoApp
+//
+//  Created by Stefan Stevanovic on 26.1.22..
+//  Copyright © 2022 Mapp Digital US, LLC. All rights reserved.
+//
+
+import Foundation
