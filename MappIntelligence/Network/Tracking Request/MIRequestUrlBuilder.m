154661e923ec6297c84fa77e336521734bed8849
Faust <stefan.stevanovic@gerniks.com>
2022-08-11 13:11:01 +0200

    send same email receiver id once user defines it

--- MappIntelligence/Network/Tracking Request/MIRequestUrlBuilder.m
+++ MappIntelligence/Network/Tracking Request/MIRequestUrlBuilder.m
@@ -72,6 +72,7 @@
 
 - (NSURL *)urlForRequest:(MITrackerRequest *)request withCustomData: (BOOL) custom{
 #define DMC_USER_ID @"dmcUserId"
+#define EMAIL_RECEIVER_ID @"emailReceiverIdUserDefaults"
     MITrackingEvent *event = [request event];
   NSString *pageNameOpt = [event pageName];
   NSURL *url;
@@ -126,7 +127,10 @@
                                                1000)]]];
   [parametrs addObject:[NSURLQueryItem queryItemWithName:@"eid"
                                                    value:properties.everId]];
-    if([[NSUserDefaults standardUserDefaults] objectForKey:DMC_USER_ID] && [[MIDefaultTracker sharedInstance] isUserMatchingEnabled]) {
+    if ([[NSUserDefaults standardUserDefaults] objectForKey:EMAIL_RECEIVER_ID]) {
+        [parametrs addObject:[NSURLQueryItem queryItemWithName:@"uc701"
+                                                         value:[[NSUserDefaults standardUserDefaults] objectForKey:EMAIL_RECEIVER_ID]]];
+    } else if([[NSUserDefaults standardUserDefaults] objectForKey:DMC_USER_ID] && [[MIDefaultTracker sharedInstance] isUserMatchingEnabled]) {
         [parametrs addObject:[NSURLQueryItem queryItemWithName:@"uc701"
                                                          value:[[NSUserDefaults standardUserDefaults] objectForKey:DMC_USER_ID]]];
     }
@@ -248,6 +252,8 @@
 -(BOOL)containsEmailReceiverId:(NSArray<NSURLQueryItem *> *)parameters {
     for (NSURLQueryItem* key in parameters) {
         if( [key.name  isEqual: @"uc701"]) {
+            [[NSUserDefaults standardUserDefaults] setObject:key.value forKey:EMAIL_RECEIVER_ID];
+            [[NSUserDefaults standardUserDefaults] synchronize];
             return YES;
         }
     }
