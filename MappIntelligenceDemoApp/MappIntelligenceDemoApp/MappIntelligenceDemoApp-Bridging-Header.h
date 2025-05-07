084b2a3ebf7503484f80a5e19c3bc9d5139a6bc3
Stefan Stevanovic <stefan.stevanovic@mapp.com>
2025-05-07 16:46:53 +0200

    update bridging header for demo app

--- MappIntelligenceDemoApp/MappIntelligenceDemoApp/MappIntelligenceDemoApp-Bridging-Header.h
+++ MappIntelligenceDemoApp/MappIntelligenceDemoApp/MappIntelligenceDemoApp-Bridging-Header.h
@@ -2,5 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <MappIntelligenceiOS/MappIntelligenceiOS.h>
+#import "MappIntelligenceiOS/MappIntelligenceiOS.h"
 #import "AppoxeeSDK.h"
